d23259745495663f967bbb36f96b2527dbe009fc
Róbert Oravec <oravec@reinto.cz>
2018-05-14 09:29:15 +0200

    added label

--- GitExample/GitExample/ViewController.swift
+++ GitExample/GitExample/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("test, I am not creative")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated. NOPE
     }
 
 
